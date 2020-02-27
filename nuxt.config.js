ee3604164ee84f6f602eeef3873024e76303fdba
Curtis Belt <curtis@curtisbelt.dev>
2020-02-27 07:05:14 -0600

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+export default {
+  head: {
+    titleTemplate: (titleChunk) =>
+      titleChunk ? `${titleChunk} - Nuxt.js Template` : 'Nuxt.js Template'
+  },
+
+  router: {
+    trailingSlash: false
+  },
+
+  css: ['assets/css/tailwind'],
+
+  plugins: [],
+
+  modules: ['@nuxtjs/axios'],
+
+  buildModules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+
+  eslint: {
+    fix: true
+  },
+
+  modern: isProd,
+
+  build: {
+    parallel: isDev,
+    cache: isDev,
+    hardSource: isDev
+  }
+}
